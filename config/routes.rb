9deb63cd66b71d923ed7a0e4bf1023a2510802ce
Brian Simmons <bsimmonsn@gmail.com>
2016-07-07 14:35:34 -0500

    added doorkeeper and stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  use_doorkeeper
 
-get 'api/posts' => 'api/posts#list'
-get 'api/post/:id' => 'api/posts#show'
+  get 'api/posts' => 'api/posts#list'
+  get 'api/post/:id' => 'api/posts#show'
+
+
+
+
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707183955) do
+ActiveRecord::Schema.define(version: 20160707190814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20160707183955) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
